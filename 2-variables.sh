86bf7b09491525b3fe0d97046f0a11be27688215
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-11 23:25:29 -0400

    Added the second script

---
+++ 2-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Jashwanth: Hello there, I am DevOps Engineer"
+echo "Yarrarapu: Hello there, I am SR Engineer"
+echo "Jashwanth: Oh, That's nice"
+echo "Yarrarapu: Nice to meet you"
+
