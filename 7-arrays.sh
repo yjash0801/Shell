04b7a884cd73f32c37a57fe1b065c7860d51d2e9
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-12 00:13:41 -0400

    Edited Arrays 7 shell file

---
+++ 7-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango" "Pineapple" "Watermelon")
+
+echo "The first fruit: ${FRUITS[0]}"
+echo "The first fruit: ${FRUITS[1]}"
+echo "The first fruit: ${FRUITS[2]}"
+echo "The first fruit: ${FRUITS[3]}"
+echo "The first fruit: ${FRUITS[4]}"
