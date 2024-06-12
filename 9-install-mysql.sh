47225731b0fe6ee030714b7fb9443c964647849c
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-12 00:53:54 -0400

    Added conditions 9 shell file

---
+++ 9-install-mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "root permission required, enter sudo su -"
+    exit 1
+else
+    echo "script executed on super user(root)"
+fi
+
+yum install mysql -y
+
+if [ $? -eq 0]
+then
+    echo "MySQL installation sucess"
+else
+    echo "MySQL installation failed"
+
+
+
