3c743deae1b979157fbfeffdf1c703ff41a29763
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-12 12:00:43 -0400

    edited the 6 datatypes shell

--- 6-datatypes.sh
+++ 6-datatypes.sh
@@ -6,3 +6,11 @@ NUM2=$2
 SUM=$(echo "$NUM1 + $NUM2" | bc)
 
 echo "Addition of 2 number: $SUM"
+
+echo "How many args passed: $#"
+
+echo "What are the agrs passed: $@"
+
+echo "Script name:: $0"
+
+echo "Script name:: $N"
