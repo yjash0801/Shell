f420b7a52ab6b1f32da01fb3c5ee291f2b704046
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-11 23:41:24 -0400

    Added variables to 4 shell file

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello there $PERSON2, I am DevOps Engineer"
+echo "$PERSON2: Hello there $PERSON1, I am SR Engineer"
+echo "$PERSON1: Oh, That's nice $PERSON2"
+echo "$PERSON2: Nice to meet you $PERSON1"
