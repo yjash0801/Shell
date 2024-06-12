7b11c9a005c95388893e7c518e2a7550a9cf2b28
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-12 00:16:30 -0400

    Edited Arrays 7 shell file

--- 7-arrays.sh
+++ 7-arrays.sh
@@ -2,12 +2,12 @@
 
 FRUITS=("Apple" "Banana" "Mango" "Pineapple" "Watermelon")
 
-echo "The first is: ${FRUITS[0]}"
-echo "The first is: ${FRUITS[1]}"
-echo "The first is: ${FRUITS[2]}"
-echo "The first is: ${FRUITS[3]}"
-echo "The first is: ${FRUITS[4]}"
+echo "The first fruit is: ${FRUITS[0]}"
+echo "The first fruit is: ${FRUITS[1]}"
+echo "The first fruit is: ${FRUITS[2]}"
+echo "The first fruit is: ${FRUITS[3]}"
+echo "The first fruit is: ${FRUITS[4]}"
 
 #To print all at once
 echo "+++++++++++++++++++++++++++"
-echo "The first is: ${FRUITS[@]}"
+echo "All fruits: ${FRUITS[@]}"
