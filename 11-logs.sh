06976619ae74d584642e36b4d3a0be52392f3390
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-12 01:41:50 -0400

    added log 11 shell file

---
+++ 11-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+DATE=$(date +%F:%H:%M:%S)
+LOGFILE="/tmp/$0-$DATE.log"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "installation $2 $R failed"
+    else
+        echo "installation $2 $G sucess"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "root permission required, enter sudo su -"
+    exit 1
+else
+    echo "script executed on super user(root)"
+fi
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "MySQL"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "git"
