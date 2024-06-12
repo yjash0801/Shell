64624bf195be224fa7b42bfb6e9c321c11a1bffd
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-11 23:27:51 -0400

    edited the 2nd shell file

--- 2-variables.sh
+++ 2-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Jashwanth: Hello there, I am DevOps Engineer"
-echo "Yarrarapu: Hello there, I am SR Engineer"
-echo "Jashwanth: Oh, That's nice"
-echo "Yarrarapu: Nice to meet you"
+echo "Jashwanth: Hello there Yarrarapu, I am DevOps Engineer"
+echo "Yarrarapu: Hello there Jashwanth, I am SR Engineer"
+echo "Jashwanth: Oh, That's nice Yarrarapu"
+echo "Yarrarapu: Nice to meet you Jashwanth"
 
