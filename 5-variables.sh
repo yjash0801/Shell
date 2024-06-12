f253cb63fe5272293d32d309235f330aba62712f
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-11 23:52:55 -0400

    Edited variables 5 shell file

--- 5-variables.sh
+++ 5-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Please enter username: $(read -s USERNAME)"
-echo "Please enter password: $(read -s PASSWORD)"
+echo "Please enter username:" $(read -s USERNAME)
+echo "Please enter password:" $(read -s PASSWORD)
 
 echo "username: $USERNAME and password: $PASSWORD"
