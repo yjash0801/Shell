cce4c40ab84757c59e824c45a54789dd81325956
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-14 14:58:03 -0400

    all commits

---
+++ 17-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE "tmp|File" | awk '{print $6F}' | cut -d % -f1)
+DISK_THRESHOL=1
+message=""
+
+R="\e[31m"
+N="\e[0m"
+
+while IFS= read line
+do
+    diskname=$(df -hT | grep -vE "tmp|File"| awk '{print $1F}')
+    if [ $line -gt 0 ]
+    then
+        message+="${R}High disk usage on${N}$diskname: usage percent: ${R}$line%${N} <br>"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $message"
