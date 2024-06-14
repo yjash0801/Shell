958f7c4930e97649d7e1c3147db35fe882d28d77
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-14 13:23:57 -0400

    all commits

--- 16-ifs.sh
+++ 16-ifs.sh
@@ -14,5 +14,5 @@ fi
 
 while IFS=":" read -r username password userid groupid User_ID_Info hdirectory shell
 do
-    echo "${G}username${N}:$username | ${G}password${N}:$username | ${G}userid${N}:$userid | ${G}groupid${N}:$groupid | ${G}User_ID_Info${N}:$User_ID_Info | ${G}hdirectory${N}:$hdirectory | ${G}shell${N}:$shell"
+    echo -e "${G}username${N}:$username | ${G}password${N}:$username | ${G}userid${N}:$userid | ${G}groupid${N}:$groupid | ${G}User_ID_Info${N}:$User_ID_Info | ${G}hdirectory${N}:$hdirectory | ${G}shell${N}:$shell"
 done < $FILE
