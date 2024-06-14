c4db322228e065d1591f9467261dda233849cf91
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-14 12:40:52 -0400

    added 14 and 15 sh file

---
+++ 14-delete-old-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+APPLOG=/tmp/shell-logs
+DATE=$(date +%F:%H:%M:%S)
+LOGDIR=/home/centos/Shell_Logs
+DELETED_LOGS=$LOGDIR/$(basename $0)-$DATE.log
+
+exec &> $LOGDIR
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d  $APPLOG ]
+then
+    echo -e "${R}The Directory doesn't exist."
+fi
+
+FILES-TO-DELETE=$(find . -type f -name "*.log" -mtime +14)
+
+while IFS= read -r lines
+do
+    echo "Deleting file: $line"
+    #rm -rf $line
+done >>> $FILES-TO-DELETE
