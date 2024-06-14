00e0a80c7402c92a3f03889c1031fa5d0b48ea72
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-14 12:46:43 -0400

    all commits

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -2,10 +2,11 @@
 
 APPLOG=/tmp/shell-logs
 DATE=$(date +%F:%H:%M:%S)
-LOGDIR=/home/centos/Shell_Logs
+LOGDIR=/home/centos/Shell_Logs/
 DELETED_LOGS=$LOGDIR/$(basename $0)-$DATE.log
 
-exec &> $LOGDIR
+
+#exec &> $LOGDIR
 
 R="\e[31m"
 G="\e[32m"
@@ -22,5 +23,5 @@ FILES_TO_DELETE=$(find . -type f -name "*.log" -mtime +14)
 while IFS= read -r lines
 do
     echo "Deleting file: $line"
-    #rm -rf $line
+    #rm -rf $line &>> $DELETED_LOGS
 done >>> $FILES_TO_DELETE
